848f19a7665580893f98e9f2dca24bdfa75fc3e7
kylepollina <kylepollina@gmail.com>
2019-11-13 13:49:02 -0500

    Preprocessing for concatenated data

--- preprocessing.py
+++ preprocessing.py
@@ -1,32 +1,43 @@
 from scipy.io import loadmat
 from sklearn.decomposition import PCA
 import numpy as np
+import math
+
+import pdb
 
 def run():
-    p1 = loadmat('p1')
-    meta = p1['meta'][0][0]
-    info = p1['info'][0]
-    data = p1['data']
-
-    total_trials = 360 
     preprocessed_data = [] 
-    n = 0
 
-    for j in range(total_trials):
-        image = get_image(data, j)
+    for i in range(1, 10):
+        filename = 'p' + str(i)
+        data = loadmat(filename)
+        meta = data['meta'][0][0]
+        info = data['info'][0]
+        images = data['data']
 
-        epoch = get_epoch(info, j)
+        total_trials = 360 
 
-        if epoch == 0 or epoch == 1 or epoch == 2:
-            preprocessed_data.append(image)
-            n += 1
+        for j in range(total_trials):
+            image = get_image(images, j)
+            epoch = get_epoch(info, j)
+
+            if epoch == 0 or epoch == 1 or epoch == 2:
+                preprocessed_data.append(image)
 
     X = preprocessed_data
     pca = PCA(n_components=100)
-    pca.fit_transform(X)
+    return pca.fit_transform(X)
 
-def get_image(data, index):
-    return data[index][0][0]
+def get_image(images, index):
+    image = images[index][0][0]
+
+    # truncate the image so each patient image is same length
+    max_image_size = 19750
+    margin = (len(image) - max_image_size) / 2
+    # center image truncation
+    truncated_image = image[math.floor(margin):][:(len(image)-math.floor(margin)-math.ceil(margin))]
+
+    return truncated_image
 
 def get_epoch(info, index):
     return info[index][4][0][0]
