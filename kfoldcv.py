27727825761a07c3bfa18271330b9a1c2f33e2d6
Eimti <eimtixa@gmail.com>
2019-11-15 14:52:59 -0500

    initial kfoldcv code

---
+++ kfoldcv.py
@@ -0,0 +1,30 @@
+from sklearn import svm
+import numpy as np
+k = 5
+
+# returns a vector of 5 mean squared errors
+def svm5F(X, y):
+	#line to change for the other model
+    clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
+	n, d = X.shape
+	z = np.zeroes(k)
+	for i in range(k):
+		#get indices of the current fold
+		startIndex = int(n * i / k)
+		endIndex = int(n * (i + 1) / k)
+		T = set(range(startIndex, endIndex))
+		S = set(range(n)) - T #set difference
+
+		#fit the model
+		#line to change for the other model
+		clf.fit(X[list(S)], y[list(S)])
+
+		#for every incorrectly predicted point, increase the error for that point by 1
+		z[i] = 0
+		for t in T:
+			if y[t] != clf.predict(X[t]):
+				z[i] += 1
+		#then divide by the number of test points for a weighted error
+		z[t] /= len(T)
+
+	return z
--- svm_.py
+++ svm_.py
@@ -1,7 +1,11 @@
 from sklearn import svm
 import preprocessing
+import kfoldcv
 
 def run():
     X, y = preprocessing.run()
     clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
     clf.fit(X, y)
+
+    #for now, just run the function. eventually, we'll be iterating through these, passing in different tuning parameters each time
+    z = kfoldcv.svm5F(X, y)
