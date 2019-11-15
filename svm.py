68f0b19e05a83acdab20fbde2dd9261fda877017
Eimti <eimtixa@gmail.com>
2019-11-15 16:37:07 -0500

    apparently i can't even merge properly

--- svm.py
+++ svm.py
@@ -5,8 +5,6 @@ import kfoldcv
 def run():
     X, y = preprocessing.run()
     clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
-<<<<<<< HEAD:svm_.py
     #for now, just run the function. eventually, we'll be iterating through these, passing in different tuning parameters each time
     z = kfoldcv.svm5F(X, y)
     return clf.fit(X, y)
->>>>>>> fb5a1783e64de30fdebc3e340368b04784853185:svm.py
