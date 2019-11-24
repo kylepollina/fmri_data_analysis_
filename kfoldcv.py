eed69b8a148b3ccf2dacd5a7993e1752ad0ead63
Eimti <eimtixa@gmail.com>
2019-11-24 15:09:26 -0500

    pca commented out, uncomment chosen version

--- kfoldcv.py
+++ kfoldcv.py
@@ -1,5 +1,6 @@
 from sklearn import svm
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.decomposition import PCA
 import numpy as np
 numFolds = 5
 
@@ -7,6 +8,9 @@ numFolds = 5
 def svm5F(X, y):
 	#line to change for the other model
 	clf = svm.SVC(gamma='scale', decision_function_shape='ovo')
+	pca = PCA(n_components = 100)
+	#uncomment if pca for entire training/validation set
+	#X = pca.fit_transform(X)
 	n, d = X.shape
 	z = np.zeroes(numFolds)
 	for i in range(numFolds):
@@ -17,8 +21,12 @@ def svm5F(X, y):
 		S = set(range(n)) - T #set difference
 
 		#fit the model
-		#line to change for the other model
-		clf.fit(X[list(S)], y[list(S)])
+		Xfold = X[list(S)]
+		yfold = y[list(S)]
+		#uncomment if pca each fold
+		#Xfold = pca.fit_transform(Xfold)
+
+		clf.fit(Xfold, yfold)
 
 		#for every incorrectly predicted point, increase the error for that point by 1
 		z[i] = 0
@@ -34,6 +42,9 @@ def knn5F(X, y, k):
 	n, d = X.shape
 	z = np.zeroes(numFolds)
 	clf = KNeighborsClassifier(n_neighbors = k)
+	pca = PCA(n_components = 100)
+	#uncomment if pca for entire training/validation set
+	#X = pca.fit_transform(X)
 	for i in range(numFolds):
 		#get indices of the current fold
 		startIndex = int(n * i / numFolds)
@@ -42,7 +53,11 @@ def knn5F(X, y, k):
 		S = set(range(n)) - T #set difference
 
 		#fit the model
-		clf.fit(X[list(S)], y[list(S)])
+		Xfold = X[list(S)]
+		yfold = y[list(S)]
+		#uncomment if pca each fold
+		#Xfold = pca.fit_transform(Xfold)
+		clf.fit(Xfold, yfold)
 
 		#for every incorrectly predicted point, increase the error for that point by 1
 		z[i] = 0
